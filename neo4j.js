9939817bb1d80535ee8bb381f10df77031e08d24
Dom Davis <dom@domdavis.com>
2014-12-23 18:47:54 +0000

    Add `buildStatement` function

--- neo4j.js
+++ neo4j.js
@@ -39,12 +39,12 @@ function Neo4j(uri) {
 // Neo4j.query(
 //     [
 //         {
-//             statement: 'MATCH (n {id: {id} }) RETURN (n)',
-//             parameters: { `id`: 123 }
+//             'statement': 'MATCH (n {id: {id} }) RETURN (n)',
+//             'parameters': { `id`: 123 }
 //         },
 //         {
-//             statement: 'MATCH (n {id: {id} }) RETURN (n)',
-//             parameters: { `id`: 124 }
+//             'statement': 'MATCH (n {id: {id} }) RETURN (n)',
+//             'parameters': { `id`: 124 }
 //         },
 //     ],
 //     function(err, results) { console.log(JSON.stringify(data); }
@@ -66,19 +66,85 @@ Neo4j.prototype.query = function(statement, parameters, callback) {
     if (Array.isArray(statement)) {
         statements = statement;
     } else {
-        statements = [{ statement: statement, parameters: parameters }];
+        statements = [{ 'statement': statement, 'parameters': parameters }];
     }
 
     var uri = neo4j + '/db/data/transaction/commit';
 
     request.post(
-        { uri: uri, json: { statements: statements } },
+        { 'uri': uri, 'json': { 'statements': statements } },
         function(err, results) {
             var data = results && results.body ? results.body.results :
                 undefined;
             callback(err, data);
         }
     );
+};
+
+// In order to simplify building statement objects the `buildStatement` function
+// is provided. It will take a string, or an array of strings, an object
+// containing client side substitutions and an object containing server side
+// parameters. In the case where the query is presented as an array of strings
+// the array is simply concatenated together with newlines.
+//
+// Client side substitutions work in a similar fashion to the server side
+// parameters except they are denoted using `${var}` rather than `{var}`, and
+// they confer no performance gains - they simply act as a convenience that
+// allows for greater parametrisation than vanilla Neo4J queries. The function
+// returns a valid statement object that can be used in a list with `query`.
+
+// For example, the following code:
+//
+// ```
+// var template = `MATCH (:${foo} {value: {value}})`;
+// var substitutions = { 'foo': 'Baz'};
+// var parameters = { 'value': 'bar' };
+// var statement = neo4j.buildStatement(template, substitutions, parameters);
+// ```
+//
+// Will yield the following object for `statement`:
+//
+// ```
+// {
+//     statement: "MATCH(:Baz {value: {value}})",
+//     parameters: { value: "bar" }
+// }
+// ```
+
+Neo4j.prototype.buildStatement = function(template, substitutions, parameters) {
+    var statement = template;
+
+    if (Array.isArray(template)) {
+        statement = template.join('\n');
+    }
+
+
+    if (!substitutions) {
+        substitutions = {};
+    }
+
+    if (!parameters) {
+        parameters = {};
+    }
+
+    for (var substitution in substitutions) {
+        /* istanbul ignore else  */
+        if (substitutions.hasOwnProperty(substitution)) {
+            var regex = new RegExp('\\$\\{' + substitution + '}', 'g');
+            statement = statement.replace(regex, substitutions[substitution]);
+        }
+    }
+
+    var matches = statement.match(/\$\{[^}]*?}/g);
+
+    if (matches) {
+        var message = 'Error, unmatched parameter';
+        message += matches.length > 1 ? 's: ' : ': ';
+        message += matches.join(', ');
+        throw new Error(message);
+    }
+
+    return { 'statement': statement, 'parameters': parameters };
 };
 
 module.exports = Neo4j;
